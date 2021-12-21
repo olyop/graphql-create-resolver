eaea0c5a85a85400fc036a123c420fb7500d6529
olyop <oliver.plummer@outlook.com>
2021-12-21 11:05:31 +1100

    Upload

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { GraphQLResolveInfo } from "graphql"
+
+type CreateResolverCheckContextFunction<C> =
+	(context: C) => void
+
+export interface CreateResolverParameter<P, A, C> {
+	args: A,
+	parent: P,
+	context: C,
+	info: GraphQLResolveInfo,
+}
+
+export type CreateResolverCallback<P, A, C, R> =
+	(props: CreateResolverParameter<P, A, C>) => R | Promise<R>
+
+export const createResolver =
+	<C = undefined>(checkContextFunction: CreateResolverCheckContextFunction<C>) =>
+		<P = undefined>() =>
+			<R, A = undefined>(callback: CreateResolverCallback<P, A, C, R>, checkContext = true) =>
+				(parent: P, args: A, context: C, info: GraphQLResolveInfo) => {
+					if (checkContext) {
+						checkContextFunction(context)
+					}
+					return callback({ parent, args, context, info })
+				}
