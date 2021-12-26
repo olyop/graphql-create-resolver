a0dc985fa05c4e41d8febcab49e5c2e3d8b68d4a
olyop <oliver.plummer@outlook.com>
2021-12-26 15:16:24 +1100

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
 import { GraphQLResolveInfo } from "graphql"
 
-export type CheckContextFunction<C> =
-	(context: C) => void
-
-export interface Parameter<P, A, C> {
-	args: A,
+export interface ParameterParent<P> {
 	parent: P,
+}
+
+export interface ParameterContext<C> {
 	context: C,
+}
+
+export interface Parameter<P, A, C>
+	extends ParameterParent<P>, ParameterContext<C> {
+	args: A,
 	info: GraphQLResolveInfo,
 }
+
+export interface CheckContextFunctionOptions<C, P = undefined>
+	extends ParameterContext<C>, Partial<ParameterParent<P>> {}
+
+export type CheckContextFunction<C> =
+	(parameter: ParameterContext<C>) => void
+
+export interface CheckGlobalContextFunctionOptions<P, C>
+	extends ParameterParent<P>, ParameterContext<C> {}
+
+export type CheckGlobalContextFunction<P, C> =
+	(parameter: CheckGlobalContextFunctionOptions<P, C>) => void
 
 export interface CheckContextOptions {
 	parentContext?: boolean,
@@ -24,7 +40,7 @@ export type Callback<P, A, C, R> =
 
 export const createRootResolver =
 	<C = undefined>(globalCheckContextFunction?: CheckContextFunction<C>) =>
-		<P = undefined>(parentCheckContextFunction?: CheckContextFunction<C>) =>
+		<P = undefined>(parentCheckContextFunction?: CheckGlobalContextFunction<P, C>) =>
 			<R, A = undefined>(callback: Callback<P, A, C, R>, options: Options) =>
 				(parent: P, args: A, context: C, info: GraphQLResolveInfo) => {
 					const {
@@ -33,11 +49,11 @@ export const createRootResolver =
 					} = options.check
 
 					if (globalContext && globalCheckContextFunction) {
-						globalCheckContextFunction(context)
+						globalCheckContextFunction({ context })
 					}
 
 					if (parentContext && parentCheckContextFunction) {
-						parentCheckContextFunction(context)
+						parentCheckContextFunction({ context, parent })
 					}
 
 					return callback({ parent, args, context, info })
